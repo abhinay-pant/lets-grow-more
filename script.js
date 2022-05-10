effcc2933f26bf75e763b942e3190fa636ed1e45
Abhinay pant <100475171+abhinay-pant@users.noreply.github.com>
2022-05-10 22:49:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
-document.querySelector("#push").onclick = function() {
-    if (document.querySelector("#newtask input").value.length == 0) {
-        alert("Please Enter a Task");
-    } else {
-        document.querySelector("#tasks").innerHTML += `
-              <div class="task">
-                  <span id="taskname">
-                      ${document.querySelector("#newtask input").value}
-                  </span>
-                  <button class="delete">
-                      <i class="far fa-trash-alt"></i>
-                  </button>
-              </div>
-          `;
-
-        var current_tasks = document.querySelectorAll(".delete");
-        for (var i = 0; i < current_tasks.length; i++) {
-            current_tasks[i].onclick = function() {
-                this.parentNode.remove();
-            };
+let screen = document.getElementById("screen");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+for (item of buttons) {
+    item.addEventListener("click", (e) => {
+        buttonText = e.target.innerText;
+        console.log("Button text is ", buttonText);
+        if (buttonText == "x") {
+            buttonText = "*";
+            screenValue += buttonText;
+            screen.value = screenValue;
+        } else if (buttonText == "C") {
+            screenValue = "";
+            screen.value = screenValue;
+        } else if (buttonText == "=") {
+            screen.value = eval(screenValue);
+        } else {
+            screenValue += buttonText;
+            screen.value = screenValue;
         }
-
-        var tasks = document.querySelectorAll(".task");
-        for (var i = 0; i < tasks.length; i++) {
-            tasks[i].onclick = function() {
-                this.classList.toggle("completed");
-            };
-        }
-
-        document.querySelector("#newtask input").value = "";
-    }
-};
+    });
+}
